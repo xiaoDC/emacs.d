a201ff2ab0607a01bcb67bdf97755bdf653b4af3
sound2gd <429354772@qq.com>
2017-04-06 21:43:12 +0800

    add evil

--- .emacs.d/elpa/tiny-menu-20161213.435/tiny-menu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "tiny-menu" "20161213.435" "Display tiny menus." '((emacs "24.4")) :url "https://github.com/aaronbieber/tiny-menu.el" :keywords '("menu" "tools"))
